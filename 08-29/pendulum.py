68c8f130ba04cbd8e80a244dcc51ce3c5ca9b0e0
tim <fitzgeraldt@gonzaga.edu>
2025-08-29 10:00:41 -0700

    add some more solvers to the example

--- 08-29/pendulum.py
+++ 08-29/pendulum.py
@@ -9,18 +9,47 @@ def pendulum_ode_linear(t, y, g, l):
     domega_dt = -(g / l) * theta
     return [dtheta_dt, domega_dt]
 
+
 # Parameters
 g = 9.81  # acceleration due to gravity (m/s^2)
 l = 1.0   # length of the pendulum (m)
-theta0 = np.pi / 4  # initial angle (radians)
+theta0 = 45*(np.pi/180) # initial angle (radians)
 omega0 = 0.0        # initial angular velocity (rad/s)  
 y0 = [theta0, omega0]
 t_span = (0, 10)  # time span for the simulation (seconds)
-t_eval = np.linspace(t_span[0], t_span[1], 100)
+t_eval = np.linspace(t_span[0], t_span[1], 1000)
 # Solve the ODE
-sol = solve_ivp(pendulum_ode_linear, t_span, y0, args=(g, l), t_eval=t_eval)
+sol_linear = solve_ivp(pendulum_ode_linear, t_span, y0, args=(g, l), t_eval=t_eval)
+
 
 # Plot the results
 plt.figure(figsize=(10, 5)) 
-plt.plot(sol.t, sol.y[0], label='Angle (rad)')
+plt.plot(sol_linear.t, sol_linear.y[0]*180/np.pi, label='Linear')
+plt.xlabel('Time (s)')
+plt.ylabel('Angle (deg)')
 
+
+## add nonlinear solution
+# def pendulum_ode_nonlinear(t, y, g, l):
+#     theta, omega = y
+#     dtheta_dt = omega
+#     domega_dt = -(g / l) * np.sin(theta)
+#     return [dtheta_dt, domega_dt]
+# sol_nonlinear = solve_ivp(pendulum_ode_nonlinear, t_span, y0, args=(g, l), t_eval=t_eval)
+# ## add nonlinear solution
+# plt.plot(
+#     sol_nonlinear.t, sol_nonlinear.y[0] * 180 / np.pi, label="Nonlinear", linestyle="--"
+# )
+# plt.legend()
+
+
+# using a different solver
+# sol_radau = solve_ivp(pendulum_ode_nonlinear, t_span, y0, args=(g, l), t_eval=t_eval, method="Radau")
+# plt.plot(
+#     sol_radau.t, sol_radau.y[0] * 180 / np.pi, label="Nonlinear Radau", linestyle=":"
+# )
+# plt.legend()
+
+
+# zoom in x-axis
+# plt.xlim(t_span[1]-10, t_span[1])
