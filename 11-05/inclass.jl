56c6c3d133146f9b5cd2d59caa5d1f1ffda85ff9
tim <fitzgeraldt@gonzaga.edu>
2025-11-05 11:20:56 -0800

    minor cleanup from class

--- 11-05/inclass.jl
+++ 11-05/inclass.jl
@@ -42,60 +42,61 @@ end
 
 function integ_adapt_h(f, x0, p, h0, tf; tol=1e-4)
 
-    n = length(x0)
-    # X = zeros(nsteps + 1, n)
-    # t = 0:h:h*nsteps
-
     X = x0'
     t = [0.]
 
     i = 1
     h = h0
-    errnm1 = NaN
+    # errnm1 = NaN
+    failcount = 0
     while t[end] < tf
 
         xn = X[i, :]
         tn = t[i]
         xn1, z = rk_adapt_step(f, xn, tn, p, h)
 
-        # TODO replace with better method
+        ## TODO replace with better method
         errn = norm(xn1 - z)
 
         if errn < tol
             X = vcat(X, xn1')
             append!(t, tn + h)
-            # println(i)
-            if i == 1
-                h = h
-                errnm1 = errn
-            else
-                # p = 2
-                # α = 0.7/p
-                # β = 0.4/p
-                # h *= (tol/errn)^α*(errnm1/tol)^β
-                # errnm1 = errn
-                fac = 0.9
-                facmin = 0.1
-                facmax = 3.
-                h *= min(facmax, max(facmin,fac* (1/errn)^(1/(2+1))))
-            end
+            
+            ## Attempt 1: PI
+            # if i == 1
+            #     h = h
+            #     errnm1 = errn
+            # else
+            #     p = 2
+            #     α = 0.7/p
+            #     β = 0.4/p
+            #     h *= (tol/errn)^α*(errnm1/tol)^β
+            #     errnm1 = errn
+            # end
+
+            ## attempt 2
+            fac = 0.9
+            facmin = 0.1
+            facmax = 3.
+            h *= min(facmax, max(facmin, fac * (1 / errn)^(1 / (2 + 1))))
+
             i += 1
         else
             h = h / 2
-            println("I failed, again")
+            failcount += 1
+            # println("I failed, again")
         end
 
     end
 
-
-    return X, t
+    return X, t, failcount
 
 end
 
 
 ##
 h0 = 0.1
-X, t = integ_adapt_h(odesys, u0, p, h0, tf, tol=1e-1)
+X, t, failcount = integ_adapt_h(odesys, u0, p, h0, tf, tol=1e-1)
 
 ## Plots
 plot(t, X, xlabel="Time t", ylabel="State")
