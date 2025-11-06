c11a930c69b4c18e9c30fb2f23f53e0af4b80c92
tim <fitzgeraldt@gonzaga.edu>
2025-11-05 19:31:44 -0800

    cleaned up code, PI is now working too

--- 11-05/inclass.jl
+++ 11-05/inclass.jl
@@ -41,6 +41,30 @@ function rk_adapt_step(f, xn, tn, p, h)
 
 end
 
+function stepsizefactor(xn1, z, xn; order=1, fac=0.9, facmin=0.1, facmax=4., Atol=1e-3, Rtol=1e-3)
+    # order: is based on the lower-order method
+
+    errn = norm(xn1 - z)
+    toln = Atol + Rtol * max(norm(xn1), norm(xn))
+    En = errn / toln
+    return En, min(facmax, max(facmin, fac * (1 / En)^(1 / (order + 1))))
+end
+
+function stepsizePI(xn1, z, xn, Enm1; order=1, Atol=1e-3, Rtol=1e-3)
+    tol = Atol .+ Rtol * maximum(abs, hcat(xn1, xn), dims=2)
+    En = 1 / sqrt(length(xn1)) * norm((xn1 - z) ./ tol)
+
+    k = order + 1
+    Ki = 0.3 / k
+    Kp = 0.4 / k
+    safetyfac = 0.9
+
+    fac = safetyfac * En^(-(Ki + Kp)) * Enm1^Kp
+
+    return En, fac
+
+end
+
 function integ_adapt_h(f, x0, p, h0, tf; tol=1e-4, maxTries=100)
 
     X = x0'
@@ -48,7 +72,7 @@ function integ_adapt_h(f, x0, p, h0, tf; tol=1e-4, maxTries=100)
 
     i = 1
     h = h0
-    # errnm1 = NaN
+    errn = 1.
     failcount = 0
     iTries = 0
     while t[end] < tf
@@ -57,52 +81,31 @@ function integ_adapt_h(f, x0, p, h0, tf; tol=1e-4, maxTries=100)
         tn = t[i]
         xn1, z = rk_adapt_step(f, xn, tn, p, h)
 
-        ## TODO replace with better method
-        errn = norm(xn1 - z)
-
+        # keep track of number of tries to compute the next step
         iTries += 1
-        order = 1
-        fac = 0.9
-        facmin = 0.1
-        facmax = 2.
-        Atol = 1e-2
-        Rtol = 1e-2
-        toln = Atol + Rtol*max(norm(xn1), norm(xn))
-        En = errn/toln
-        println( (1/En)^(1/(order+1)) )
-        # println(min(facmax, max(facmin, fac * (1 / errn)^(1 / (order + 1)))))
-        h *= min(facmax, max(facmin, fac * (1/En)^(1/(order+1)) ) )
 
-        if En < 1 
+        # compute error estimate and next step size
+        errn, fac = stepsizefactor(xn1, z, xn, Atol=tol, Rtol=0) 
+
+        # use PI step control
+        # errn, fac = stepsizePI(xn1, z, xn, errn, Atol=tol, Rtol=0)
+        
+        # update step size
+        h *= fac
+
+        # compare normalized error to 1
+        if errn < 1
             X = vcat(X, xn1')
             append!(t, tn + h)
-            iTries = 0
-            
-            ## Attempt 1: PI
-            # if i == 1
-            #     h = h
-            #     errnm1 = errn
-            # else
-            #     p = 2
-            #     α = 0.7/p
-            #     β = 0.4/p
-            #     h *= (tol/errn)^α*(errnm1/tol)^β
-            #     errnm1 = errn
-            # end
-
-            ## attempt 2
-            # fac = 0.9
-            # facmin = 0.1
-            # facmax = 3.
-            # h *= min(facmax, max(facmin, fac * (1 / errn)^(1 / (2 + 1))))
-
+            iTries = 0 # reset the local 'try' counter
             i += 1
         else
             failcount += 1
         end
 
+        # kill the loop if we aren't making progress
         if iTries >= maxTries
-            error(@sprintf("Failed to make progress\n  i=%d\n  t=%f\n  h=%f\n ", i, t[i],h))
+            error(@sprintf("Failed to make progress\n  i=%d\n  t=%f\n  h=%f\n ", i, t[i], h))
         end
 
     end
